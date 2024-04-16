5a529050551653e2109019afa9090516fb8c6274
Paguem Tagang Michele Sephora <152933754+Mpag10@users.noreply.github.com>
2024-04-15 21:13:23 -0400

    Add files via upload

---
+++ validationUtilisateur.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { required, email, min } from 'vee-validate/rules';
+
+
+export const nomValidationRule = { required, min: min(2) };
+export const emailValidationRule = { required, email };
+export const motDePasseValidationRule = { required };
+export const roleIdValidationRule = { required };
+</script>
